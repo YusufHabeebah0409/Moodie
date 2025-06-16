9d61aa5cba6b369fb4438ae297ed2eb9740a4052
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-06-16 16:54:31 +0100

    Add script.js for Firebase initialization and update index.html to include script

---
+++ script.js
@@ -0,0 +1,19 @@
+// Imports
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+
+  // Firebase setup
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZT_tQMjE86hCoSRIaMxlYiV1lBe-zHC8",
+    authDomain: "moodie-802c2.firebaseapp.com",
+    projectId: "moodie-802c2",
+    storageBucket: "moodie-802c2.firebasestorage.app",
+    // messagingSenderId: "316295030861",
+    // appId: "1:316295030861:web:945089aa0bd9cdcbbad404"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  console.log(auth);
